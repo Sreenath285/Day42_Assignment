04b587215aaf6d865afd69abcb5d7a5567b1f888
Sreenath285 <sreenath.285@gmail.com>
2022-04-10 15:36:27 +0530

    Created html input form, validating email

---
+++ InputForm/input_form_script.js
@@ -0,0 +1,31 @@
+const nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+const phoneNumberRegex = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
+const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
+const passwordRegex = RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$");
+
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function() {
+    if (nameRegex.test(text.value)) {
+        textError.textContent = "";
+    } else {
+        textError.textContent = "Incorrect name";
+    }
+});
+
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() {
+    output.textContent = salary.value;
+});
+
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function() {
+    if (emailRegex.test(email.value)) {
+        emailError.textContent = "";
+    } else {
+        emailError.textContent = "Incorrect email";
+    }
+});
